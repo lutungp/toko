d9c74f33113345c2228fda6b2f8f6fc17f6daf6f
lutungp <lntngp19@gmail.com>
2020-06-22 11:42:43 +0700

    [UPDATE] using bloc

--- lib/pages/MainPage.dart
+++ lib/pages/MainPage.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mystore/bloc/navigation_bloc/navigation_bloc.dart';
+import 'package:mystore/pages/dashboard_layout/menu.dart';
+import 'package:mystore/pages/dashboard_layout/dashboard.dart';
+import 'package:mystore/pages/ui/my_cards.dart';
 
 final Color backgroundColor = Color(0xFF4A4A58);
 class MainPage extends StatefulWidget {
@@ -31,6 +36,17 @@ class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin
     super.dispose();
   }
 
+  void onMenuTap(){
+    setState(() {
+      if (isCollapsed)
+        _controller.forward();
+      else
+        _controller.reverse();
+
+      isCollapsed = !isCollapsed;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -39,133 +55,26 @@ class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin
 
     return Scaffold(
       backgroundColor: backgroundColor,
-      body: Stack(
-        children: <Widget>[
-          menu(context),
-          dashboard(context),
-        ],
-      ),
-    );
-  }
-
-  Widget menu(context) {
-    return SlideTransition(
-      position: _slideAnimation,
-      child: ScaleTransition(
-        scale: _menuScaleAnimation,
-        child: Padding(
-          padding: const EdgeInsets.only(left: 16.0),
-          child: Align(
-            alignment: Alignment.centerLeft,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text("Dashboard", style: TextStyle(color: Colors.white, fontSize: 22)),
-                SizedBox(height: 10),
-                Text("Messages", style: TextStyle(color: Colors.white, fontSize: 22)),
-                SizedBox(height: 10),
-                Text("Utility Bills", style: TextStyle(color: Colors.white, fontSize: 22)),
-                SizedBox(height: 10),
-                Text("Funds Transfer", style: TextStyle(color: Colors.white, fontSize: 22)),
-                SizedBox(height: 10),
-                Text("Branches", style: TextStyle(color: Colors.white, fontSize: 22)),
-              ],
+      body: BlocProvider<NavigationBloc>(
+        create : (context) => NavigationBloc(onMenuTap: onMenuTap),
+        child: Stack(
+          children: <Widget>[
+            Menu(
+              slideAnimation: _slideAnimation, 
+              menuAnimation : _menuScaleAnimation
             ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget dashboard(context) {
-    return AnimatedPositioned(
-      duration: duration,
-      top: 0,
-      bottom: 0,
-      left: isCollapsed ? 0 : 0.6 * screenWidth,
-      right: isCollapsed ? 0 : -0.2 * screenWidth,
-      child: ScaleTransition(
-        scale: _scaleAnimation,
-        child: Material(
-          animationDuration: duration,
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-          elevation: 8,
-          color: backgroundColor,
-          child: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            physics: ClampingScrollPhysics(),
-            child: Container(
-              padding: const EdgeInsets.only(left: 16, right: 16, top: 48),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      InkWell(
-                        child: Icon(Icons.menu, color: Colors.white),
-                        onTap: () {
-                          setState(() {
-                            if (isCollapsed)
-                              _controller.forward();
-                            else
-                              _controller.reverse();
-
-                            isCollapsed = !isCollapsed;
-                          });
-                        },
-                      ),
-                      Text("My Cards", style: TextStyle(fontSize: 24, color: Colors.white)),
-                      Icon(Icons.settings, color: Colors.white),
-                    ],
-                  ),
-                  SizedBox(height: 50),
-                  Container(
-                    height: 200,
-                    child: PageView(
-                      controller: PageController(viewportFraction: 0.8),
-                      scrollDirection: Axis.horizontal,
-                      pageSnapping: true,
-                      children: <Widget>[
-                        Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 8),
-                          color: Colors.redAccent,
-                          width: 100,
-                        ),
-                        Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 8),
-                          color: Colors.blueAccent,
-                          width: 100,
-                        ),
-                        Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 8),
-                          color: Colors.greenAccent,
-                          width: 100,
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Text("Transactions", style: TextStyle(color: Colors.white, fontSize: 20),),
-                  ListView.separated(
-                    physics: ClampingScrollPhysics(),
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) {
-                        return ListTile(
-                          title: Text("Macbook"),
-                          subtitle: Text("Apple"),
-                          trailing: Text("-2900"),
-                        );
-                    }, separatorBuilder: (context, index) {
-                      return Divider(height: 16);
-                    }, itemCount: 10)
-                ],
-              ),
+            Dashboard(
+              duration: duration,
+              onMenuTap: onMenuTap,
+              scaleAnimation: _scaleAnimation,
+              isCollapsed: isCollapsed,
+              screenWidth: screenWidth,
+              child: BlocBuilder<NavigationBloc, NavigationStates>(builder: (context,
+                  NavigationStates navigationState,) {
+                return navigationState as Widget;
+              }),
             ),
-          ),
+          ],
         ),
       ),
     );
